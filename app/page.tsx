b220e86591bbc32456af80bb081c65760415c249
thecrankytech <68920705+thecrankytech@users.noreply.github.com>
2024-11-20 13:54:58 +0000

    remove import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(Number(localStorage.count || 0));
